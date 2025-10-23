b20b83c148ff5078b7d59363763d449ba111dc2f
Wilker <neoxextreme@gmail.com>
2025-10-23 12:31:35 -0400

    adicionado dto fixtures para testes

--- src/main/java/com/wilker/sms_api/infrastructure/dto/response/SmsMessageResponseDTO.java
+++ src/main/java/com/wilker/sms_api/infrastructure/dto/response/SmsMessageResponseDTO.java
@@ -8,7 +8,7 @@ public record SmsMessageResponseDTO (
 
         Long id,
         String phoneNumber,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
         StatusEnvioEnum statusEnvioEnum,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
         LocalDateTime sentAt) {
 }
